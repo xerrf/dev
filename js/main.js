5eeb0d215a098700650544353fc92068d96ce068
Ben <beve3854@colorado.edu>
2019-02-13 10:07:03 -0700

    figured out how to have the thumbnails have a hover state

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function (){
 
-    //Makes the height of an element match its width. Used for the h2 elements
+    //Makes the height of an element match its width. Used for the figure and h2 elements
     $(function() {
         var dynamicBox = $(".square");
         var width = dynamicBox.width();
 
         dynamicBox.css("height", width);
+        $("h2.square").css("width", width); //makes it so h2 size is the same as the figure behind it
     })
 });
