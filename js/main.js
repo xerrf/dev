4cfea1d3a170abb29d542498772198b080877372
Ben <beve3854@colorado.edu>
2019-02-13 20:18:07 -0700

    finished basics

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function (){
 
-    //Makes the height of an element match its width. Used for the figure and h2 elements
+    //Makes the height of an element match its width. Used for the figure, h2, and p elements within main
     $(function() {
         var dynamicBox = $(".square");
         var width = dynamicBox.width();
 
         dynamicBox.css("height", width);
         $("h2.square").css("width", width); //makes it so h2 size is the same as the figure behind it
+        $("p.description").css("height", width);    //makes the p a squared
     })
 });
